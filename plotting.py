e9d6409c9fcd2e7d5be99ac28ff87cb62507b65f
Sevy Harris <harris.se@northeastern.edu>
2022-08-11 08:52:40 -0400

    add a plotting module to hold frequently used code snippets

---
+++ plotting.py
@@ -0,0 +1,27 @@
+# useful functions for plotting
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def plot_kinetics(rxns, labels=None):
+    """Function for plotting reaction kinetics
+    Takes in a list of RMG reactions (rmgpy.reaction.Reaction) or a single reaction
+    """
+    plt.xlabel('1000 / T (K^-1)')
+    plt.ylabel('ln(k)')
+
+    if type(rxns) != list:
+        rxns = [rxns]
+    
+    T = np.linspace(300, 3000, 1001)
+    for rxn in rxns:
+        k = np.zeros(len(T))
+        for i in range(0, len(T)):
+            k[i] = rxn.get_rate_coefficient(T[i], 101325)
+        plt.plot(1000.0 / T, np.log(k))
+
+    if labels:
+        plt.legend(labels)
+    plt.show()
+
