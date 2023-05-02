c3692310a8b6cf37e7d4417f2b0524bb7a07b106
Sevy Harris <harris.se@northeastern.edu>
2023-05-02 07:49:37 -0400

    add code to quickly find valid TS

---
+++ arkane/report_ts_logs.py
@@ -0,0 +1,37 @@
+# script to report the lowest energy TS of the given logs
+import sys
+import glob
+import arkane.ess.gaussian
+
+
+log_search_string = sys.argv[1]
+
+log_files = glob.glob(log_search_string)
+
+energies = []
+valid_TS = []
+
+best_option = None
+lowest_energy = 1e10
+
+for logfile in log_files:
+    g_reader = arkane.ess.gaussian.GaussianLog(logfile)
+    energy = g_reader.load_energy()
+    energies.append(energy)
+
+    # get a list of the vibrational frequencies
+    freq = g_reader.load_negative_frequency()
+    if freq < -500:
+        valid_TS.append(True)
+    else:
+        valid_TS.append(False)
+
+    if energy < lowest_energy:
+        lowest_energy = energy
+        best_option = logfile
+
+# print the results of each TS
+for i, logfile in enumerate(log_files):
+    print('TS: {} Energy: {} Valid: {}'.format(logfile, energies[i], valid_TS[i]))
+
+print('The lowest energy TS is: {}'.format(best_option))
