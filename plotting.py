8f514b91e23fe6246cd1236855b6f9d7fef236f8
Sevy Harris <harris.se@northeastern.edu>
2023-05-10 11:06:40 -0400

    add thermo plots

--- plotting.py
+++ plotting.py
@@ -13,7 +13,7 @@ def plot_kinetics(rxns, labels=None):
 
     if type(rxns) != list:
         rxns = [rxns]
-    
+
     T = np.linspace(300, 3000, 1001)
     for rxn in rxns:
         k = np.zeros(len(T))
@@ -25,3 +25,38 @@ def plot_kinetics(rxns, labels=None):
         plt.legend(labels)
     plt.show()
 
+
+def plot_thermos(thermos, labels=None):
+    if type(thermos) != list:
+        thermos = [thermos]
+    if labels is None:
+        labels = ['' for t in thermos]
+    fig, ax = plt.subplots(1, 3)
+    fig.set_size_inches(12, 3)
+    fig.tight_layout()
+    ax[0].set_xlabel('Temperature (K)')
+    ax[0].set_ylabel('H (kJ / mol)')
+    ax[0].set_title('Enthalpy vs. Temperature')
+    ax[1].set_xlabel('Temperature (K)')
+    ax[1].set_ylabel('S (kJ / mol K)')
+    ax[1].set_title('Entropy vs. Temperature')
+    ax[2].set_xlabel('Temperature (K)')
+    ax[2].set_ylabel('Cp (kJ / mol K)')
+    ax[2].set_title('Heat Capacity vs. Temperature')
+    T = np.linspace(300, 3000, 1001)
+    for thermo in thermos:
+        H = np.zeros(len(T))
+        S = np.zeros(len(T))
+        Cp = np.zeros(len(T))
+        for i in range(0, len(T)):
+            H[i] = thermo.get_enthalpy(T[i]) / 1000.0
+            S[i] = thermo.get_entropy(T[i]) / 1000.0
+            Cp[i] = thermo.get_heat_capacity(T[i]) / 1000.0
+        ax[0].plot(T, H)
+        ax[1].plot(T, S)
+        ax[2].plot(T, Cp)
+    ax[0].legend(labels)
+    ax[1].legend(labels)
+    ax[2].legend(labels)
+    plt.subplots_adjust(wspace=0.25)
+    plt.show()
