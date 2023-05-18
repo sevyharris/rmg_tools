37f750ef7fed2aab4e5bdc9b263f276eff84275d
Sevy Harris <harris.se@northeastern.edu>
2023-05-18 10:27:58 -0400

    fixed bug in fix duplicates where it was incorrectly deleting DUPLICATE marks

--- fix_duplicates.py
+++ fix_duplicates.py
@@ -20,13 +20,63 @@ for i, line in enumerate(lines):
 # print(reactions)
 reaction_names = [rxn[1].split()[0] for rxn in reactions]
 
+
+def same_reaction(name1, name2):
+    # name1 = name1.split()[0]
+    # name1 = name1.split()[0]
+    reversible1 = False
+    reversible2 = False
+    # TODO manage reversibility
+
+    half_tokens1 = name1.split("=>")
+    if len(half_tokens1) == 1:
+        half_tokens1 = name1.split("=")
+        reversible1 = True
+    if len(half_tokens1) == 1:
+        raise ValueError
+
+    half_tokens2 = name2.split("=>")
+    if len(half_tokens2) == 1:
+        half_tokens2 = name2.split("=")
+        reversible2 = True
+    if len(half_tokens2) == 1:
+        raise ValueError
+
+    # TODO case where one says A + A and the other says 2A
+
+    reactants_tokens1 = set(half_tokens1[0].split('+'))
+    reactants_tokens2 = set(half_tokens2[0].split('+'))
+
+    products_tokens1 = set(half_tokens1[1].split('+'))
+    products_tokens2 = set(half_tokens2[1].split('+'))
+
+    for r in reactants_tokens1:
+        if r[0] == '2':
+            raise NotImplementedError
+
+    duplicate = False
+    if reactants_tokens1 == reactants_tokens2 and products_tokens1 == products_tokens2:
+        duplicate = True
+    elif reversible1 and reversible2 and \
+            reactants_tokens1 == products_tokens2 and products_tokens1 == reactants_tokens2:
+        duplicate = True
+
+    return duplicate
+
+
 duplicate_names = set()
 duplicates = []
 for i in range(0, len(reaction_names)):
     for j in range(0, i):
-        if reaction_names[i] == reaction_names[j]:
+        # if reaction_names[i] == reaction_names[j]:
+        if same_reaction(reaction_names[i], reaction_names[j]):
             duplicates.append((j, i))
+            print('Found duplicates:')
+            print('\t', reaction_names[i])
+            print('\t', reaction_names[j])
+            print()
             duplicate_names.add(reaction_names[j])
+            duplicate_names.add(reaction_names[i])
 
 
 def marked_duplicate(reaction_number):
@@ -34,15 +84,31 @@ def marked_duplicate(reaction_number):
     for a in range(rxn1_line, min(rxn1_line + 20, len(lines))):
         if lines[a].strip() == 'DUPLICATE':
             return True
-        elif lines[a].strip() == '':
+        elif a > rxn1_line and '=' in lines[a]:
             return False
-        elif lines[a][0] != '!' and '=' in lines[a][0]:
-            return False
+        # elif lines[a].strip() == '':
+        #     return False
+        # elif lines[a][0] != '!' and '=' in lines[a][0]:
+        #     return False
     return False
+
+    # rxn1_line = reactions[reaction_number][0]
+    # for a in range(rxn1_line, min(rxn1_line + 20, len(lines))):
+    #     if lines[a].strip() == 'DUPLICATE':
+    #         return True
+    #     elif lines[a].strip() == '':
+    #         return False
+    #     elif lines[a][0] != '!' and '=' in lines[a][0]:
+    #         return False
+    # return False
 
 
 def mark_as_duplicate(reaction_number):
     # figure out where to insert
+
+    print('Marking duplicate:')
+    print('\t', lines[reactions[reaction_number][0]])
+    print()
     rxn1_line = reactions[reaction_number][0]
     for a in range(rxn1_line + 1, min(rxn1_line + 20, len(lines))):
         if lines[a].strip() == '':
@@ -53,6 +119,8 @@ def mark_as_duplicate(reaction_number):
         else:
             lines.insert(a, 'DUPLICATE\n')
             break
+    else:
+        raise ValueError
 
     # recalculate reactions and reaction_names
     reaction_number = 0
