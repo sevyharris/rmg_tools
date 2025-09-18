3f690f2947a0d61115f20dcebfd977d0fac469cc
Sevy Harris <harris.se@northeastern.edu>
2025-09-18 16:18:16 -0400

    add updated remove duplicates script

---
+++ duplicates/fix_duplicates_seed.py
@@ -0,0 +1,197 @@
+# Script to fix incorrectly marked duplicate reactions for a chemkin file
+
+import os
+import sys
+
+
+chemkin_file = sys.argv[1]
+
+# I know it's bad form, but
+# lines, reactions, and reaction_names are global variables
+with open(chemkin_file, 'r') as f:
+    lines = f.readlines()
+
+reactions = []
+for i, line in enumerate(lines):
+    if line[0] == '!':
+        continue
+    trimmed_line = line.split('!')[0]  # remove anything that comes after a comment
+    if '=' in trimmed_line:
+        reactions.append((i, line))
+    # if line[0] != '!' and '=' in line:
+    #     reactions.append((i, line))
+
+print(f'Found {len(reactions)} reactions')
+# print(reactions)
+reaction_names = [rxn[1].split()[0] for rxn in reactions]
+
+
+def same_reaction(name1, name2):
+    # name1 = name1.split()[0]
+    # name1 = name1.split()[0]
+    reversible1 = False
+    reversible2 = False
+    # TODO manage reversibility
+
+    half_tokens1 = name1.split("=>")
+    if len(half_tokens1) == 1:
+        half_tokens1 = name1.split("=")
+        reversible1 = True
+    if len(half_tokens1) == 1:
+        print('1:\t', name1)
+        print('2:\t', name2)
+        raise ValueError
+
+    half_tokens2 = name2.split("=>")
+    if len(half_tokens2) == 1:
+        half_tokens2 = name2.split("=")
+        reversible2 = True
+    if len(half_tokens2) == 1:
+        raise ValueError
+
+    # TODO case where one says A + A and the other says 2A
+
+    reactants_tokens1 = set(half_tokens1[0].split('+'))
+    reactants_tokens2 = set(half_tokens2[0].split('+'))
+
+    products_tokens1 = set(half_tokens1[1].split('+'))
+    products_tokens2 = set(half_tokens2[1].split('+'))
+
+    for r in reactants_tokens1:
+        if r[0] == '2':
+            raise NotImplementedError
+
+    duplicate = False
+    if reactants_tokens1 == reactants_tokens2 and products_tokens1 == products_tokens2:
+        duplicate = True
+    elif reversible1 and reversible2 and \
+            reactants_tokens1 == products_tokens2 and products_tokens1 == reactants_tokens2:
+        duplicate = True
+
+    return duplicate
+
+
+duplicate_names = set()
+duplicates = []
+for i in range(0, len(reaction_names)):
+    for j in range(0, i):
+        # if reaction_names[i] == reaction_names[j]:
+        if same_reaction(reaction_names[i], reaction_names[j]):
+            duplicates.append((j, i))
+            #print('Found duplicates:')
+            #print('\t', reaction_names[i])
+            #print('\t', reaction_names[j])
+            #print()
+            duplicate_names.add(reaction_names[j])
+            duplicate_names.add(reaction_names[i])
+
+
+def marked_duplicate(reaction_number):
+    rxn1_line = reactions[reaction_number][0]
+    for a in range(rxn1_line, min(rxn1_line + 20, len(lines))):
+        if lines[a].strip() == 'DUPLICATE':
+            return True
+        elif a > rxn1_line and '=' in lines[a]:
+            return False
+        # elif lines[a].strip() == '':
+        #     return False
+        # elif lines[a][0] != '!' and '=' in lines[a][0]:
+        #     return False
+    return False
+
+    # rxn1_line = reactions[reaction_number][0]
+    # for a in range(rxn1_line, min(rxn1_line + 20, len(lines))):
+    #     if lines[a].strip() == 'DUPLICATE':
+    #         return True
+    #     elif lines[a].strip() == '':
+    #         return False
+    #     elif lines[a][0] != '!' and '=' in lines[a][0]:
+    #         return False
+    # return False
+
+
+def mark_as_duplicate(reaction_number):
+
+    assert reaction_number < len(reactions)
+    # figure out where to insert
+
+    #print('Marking duplicate:')
+    #print('\t', lines[reactions[reaction_number][0]])
+    #print()
+    rxn1_line = reactions[reaction_number][0]
+    for a in range(rxn1_line + 1, min(rxn1_line + 20, len(lines))):
+        if lines[a].strip() == '':
+            lines.insert(a, 'DUPLICATE\n')
+            break
+        elif lines[a].strip()[-1] == '/':
+            continue
+        else:
+            lines.insert(a, 'DUPLICATE\n')
+            break
+    else:
+        raise ValueError
+
+    # recalculate reactions and reaction_names
+    reaction_number = 0
+    for i, line in enumerate(lines):
+        if line[0] == '!':
+            continue
+        trimmed_line = line.split('!')[0]  # remove anything that comes after a comment
+        if '=' in trimmed_line:
+            reactions[reaction_number] = ((i, line))
+            reaction_number += 1
+
+
+def delete_duplicate_mark(reaction_number):
+    # figure out where to delete
+    rxn1_line = reactions[reaction_number][0]
+    for a in range(rxn1_line, min(rxn1_line + 20, len(lines))):
+        if lines[a].strip() == 'DUPLICATE' or 'DUPLICATE' in lines[a].strip().split():
+            del(lines[a])
+            break
+
+    # recalculate reactions and reaction_names -- happens in 3 places
+    reaction_number = 0
+    for i, line in enumerate(lines):
+        if line[0] == '!':
+            continue
+        trimmed_line = line.split('!')[0]  # remove anything that comes after a comment
+        if '=' in trimmed_line:
+            reactions[reaction_number] = ((i, line))
+            reaction_number += 1
+
+
+def comment_out_reaction(reaction_number):
+
+    # if marked_duplicate(reaction_number):
+    #     delete_duplicate_mark(reaction_number)
+    rxn1_line = reactions[reaction_number][0]
+    print(f'modifying: {lines[rxn1_line]}')
+    lines[rxn1_line] = '! ' + lines[rxn1_line]
+
+    if 'DUPLICATE' in lines[rxn1_line + 1]:
+        lines[rxn1_line + 1] = '! ' + lines[rxn1_line + 1]
+
+
+
+# Add missing DUPLICATE markers
+for pair in duplicates:
+    if not marked_duplicate(pair[0]):
+
+        # delete pair 1
+        comment_out_reaction(pair[1])
+        print(pair[1])
+
+        # mark_as_duplicate(pair[0])
+    # if not marked_duplicate(pair[1]):
+    #     mark_as_duplicate(pair[1])
+
+# Get rid of incorrectly marked duplicates
+for i in range(0, len(reactions)):
+    if marked_duplicate(i) and reaction_names[i] not in duplicate_names:
+        delete_duplicate_mark(i)
+
+# save the fixed file
+chemkin_file_out = chemkin_file[:-4] + '_fixed' + chemkin_file[-4:]
+with open(chemkin_file_out, 'w') as f:
+    f.writelines(lines)
--- duplicates/remove_duplicates.py
+++ duplicates/remove_duplicates.py
@@ -1,4 +1,4 @@
-# script to remove duplicates
+# script to remove duplicates -- not sure why it doesn't work
 import os
 import sys
 import rmgpy.chemkin
@@ -9,6 +9,10 @@ chemkin_file = sys.argv[1]
 sp_dict = os.path.join(os.path.dirname(chemkin_file), 'species_dictionary.txt')
 species_list, reaction_list = rmgpy.chemkin.load_chemkin_file(chemkin_file, sp_dict, check_duplicates=False)
 
+verbose = False
+if len(sys.argv) > 2:
+    if sys.argv[2] in ['verbose=True', '--verbose', '--verbose=True']:
+        verbose = True
 
 # count the bad duplicate pairs
 bad_duplicate_pairs = []
@@ -19,16 +23,117 @@ for i in range(len(reaction_list)):
             if not (reaction_list[i].duplicate and reaction_list[j].duplicate):
                 bad_duplicate_pairs.append([i, j])
 
-
     if len(reaction_list[i].reactants) == 1 and len(reaction_list[i].products) == 1:
         # check for case of A (+M) <=> B (+M) not matched to 2A (+M) <=> 2B (+M) 
-        
+
         other_reaction = rmgpy.reaction.Reaction()
         other_reaction.reactants = [reaction_list[i].reactants[0], reaction_list[i].reactants[0]]
         other_reaction.products = [reaction_list[i].products[0], reaction_list[i].products[0]]
-        
+
         for j in range(len(reaction_list)):
             if j == i:
                 continue
-            if reaction_list[j].is_isomorphic(other_reaction):
-                bad_duplicate_pairs.append([i, j])
+            if reaction_list[j].is_isomorphic(other_reaction) and not (reaction_list[i].duplicate and reaction_list[j].duplicate):
+                reaction_list[i].duplicate = True
+                reaction_list[j].duplicate = True
+                # bad_duplicate_pairs.append([i, j])
+
+
+print(f'Found {len(bad_duplicate_pairs)} bad duplicate pairs')
+print(bad_duplicate_pairs)
+
+remove_list = []  # indices of reactions to remove
+remove_list_items = []  # Reaction objects of the reactions to remove
+repaired_list = []
+for bad_duplicate_pair in bad_duplicate_pairs:
+    i = bad_duplicate_pair[0]
+    j = bad_duplicate_pair[1]
+
+    # one PDEP and one library -- this is okay
+    if isinstance(reaction_list[i], rmgpy.rmg.pdep.PDepReaction) and \
+            isinstance(reaction_list[j], rmgpy.data.kinetics.library.LibraryReaction):
+        # mark them as duplicates
+        reaction_list[i].duplicate = True
+        reaction_list[j].duplicate = True
+        repaired_list.append([i, j])
+    elif not isinstance(reaction_list[i], rmgpy.data.kinetics.library.LibraryReaction) and \
+            isinstance(reaction_list[j], rmgpy.rmg.pdep.PDepReaction):
+        # mark them as duplicates
+        reaction_list[i].duplicate = True
+        reaction_list[j].duplicate = True
+        repaired_list.append([i, j])
+
+    # one PDEP and one library -- this is okay
+    if isinstance(reaction_list[i].kinetics, rmgpy.kinetics.chebyshev.Chebyshev) and \
+            isinstance(reaction_list[j], rmgpy.data.kinetics.library.LibraryReaction):
+        # mark them as duplicates
+        reaction_list[i].duplicate = True
+        reaction_list[j].duplicate = True
+        repaired_list.append([i, j])
+    elif not isinstance(reaction_list[i], rmgpy.data.kinetics.library.LibraryReaction) and \
+            isinstance(reaction_list[j].kinetics, rmgpy.kinetics.chebyshev.Chebyshev):
+        # mark them as duplicates
+        reaction_list[i].duplicate = True
+        reaction_list[j].duplicate = True
+        repaired_list.append([i, j])
+
+    # one library and one family -- this is bad. remove
+    elif isinstance(reaction_list[i], rmgpy.data.kinetics.library.LibraryReaction) and \
+            not isinstance(reaction_list[j], rmgpy.data.kinetics.library.LibraryReaction):
+        remove_list.append(j)
+        remove_list_items.append(reaction_list[j])
+    elif not isinstance(reaction_list[i], rmgpy.data.kinetics.library.LibraryReaction) and \
+            isinstance(reaction_list[j], rmgpy.data.kinetics.library.LibraryReaction):
+        remove_list.append(i)
+        remove_list_items.append(reaction_list[i])
+
+    # one PDEP and one family -- this is okay
+    elif isinstance(reaction_list[i], rmgpy.rmg.pdep.PDepReaction) and \
+            not isinstance(reaction_list[j], rmgpy.rmg.pdep.PDepReaction):
+        # mark them as duplicates
+        reaction_list[i].duplicate = True
+        reaction_list[j].duplicate = True
+        repaired_list.append([i, j])
+    elif not isinstance(reaction_list[i], rmgpy.rmg.pdep.PDepReaction) and \
+            isinstance(reaction_list[j], rmgpy.rmg.pdep.PDepReaction):
+        # mark them as duplicates
+        reaction_list[i].duplicate = True
+        reaction_list[j].duplicate = True
+        repaired_list.append([i, j])
+
+    # two PDEPs -- this is probably wrong
+    elif isinstance(reaction_list[i], rmgpy.rmg.pdep.PDepReaction) and \
+            isinstance(reaction_list[j], rmgpy.rmg.pdep.PDepReaction):
+        # remove the one with more reactants
+        if len(reaction_list[i].reactants) > reaction_list[j].reactants:
+            remove_list.append(i)
+            remove_list_items.append(reaction_list[i])
+        elif len(reaction_list[i].reactants) < reaction_list[j].reactants:
+            remove_list.append(j)
+            remove_list_items.append(reaction_list[j])
+        else:  # remove j by default because it was added second
+            remove_list.append(j)
+            remove_list_items.append(reaction_list[j])
+
+    else:
+        print('Unknown kind of bad pair -- not implemented')
+        print(type(reaction_list[i]))
+        print(type(reaction_list[j]))
+        print()
+
+# actually remove the items
+for i, item in enumerate(remove_list_items):
+    reaction_list.remove(item)
+    if verbose:
+        print(f'Removing reaction {remove_list[i]}: {item}')
+
+if verbose:
+    for pair in repaired_list:
+        print(f'Repaired {pair}')
+
+print(f'Removed {len(remove_list_items)}')
+print(f'Repaired {len(repaired_list)}')
+
+# save the resulting chemkin file
+out_chemkin = chemkin_file.replace('.inp', '_fixed.inp')
+rmgpy.chemkin.save_chemkin_file(out_chemkin, species_list, reaction_list)
